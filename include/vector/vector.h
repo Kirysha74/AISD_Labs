2454f1281901184c1a5eceda20eb455f2cbe48da
Kirill Trofimov <kaha2015-2016@mail.ru>
2023-11-08 10:23:24 +0400

    Pre-final version

--- include/vector/vector.h
+++ include/vector/vector.h
@@ -1,178 +1,278 @@
-#pragma once
+#pragma once 
 #include <iostream>
 #include <complex>
 
 using namespace std;
 
-template <typename T>
-class Vector {
-private:
-	int _size;
-	T* _vector;
-public:
+namespace vector {
 
-	int get_size() const {
-		return _size;
-	}
+	template <typename T>
+	class Vector {
+	private:
+		int _size;
+		T* _vector;
+		inline static const double EPSILION = 0.0001;
+	public:
 
-	Vector<T>& operator=(const Vector<T>& rhs) {
-		_size = rhs.get_size();
-		_vector = new T[_size];
+		int get_size() const {
+			return _size;
+		}
+
+		Vector<T>& operator=(const Vector<T>& rhs) {
+			_size = rhs.get_size();
+			_vector = new T[_size];
+			for (int i = 0; i < _size; i++) {
+				_vector[i] = rhs[i];
+			}
+		}
+
+		T& operator[](int i) const {
+			if (i < 0 || i >= _size) {
+				throw exception("Incorrect index");
+			}
+			return _vector[i];
+		}
+
+		bool operator==(const Vector<T> rhs) {
+			if (_size != rhs.get_size()) {
+				throw exception("Two vectors of different sizes were transmitted");
+			}
+			for (int i = 0; i < _size; i++) {
+				if (abs(_vector[i] - rhs[i]) > EPSILION) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		Vector() : _size(2), _vector(new T[_size]) {
+			for (int i = 0; i < _size; i++) {
+				_vector[i] = rand() + 0.000001 * rand();
+			}
+		}
+
+		~Vector() {
+			delete [] _vector;
+			_vector = nullptr;
+			_size = 0;
+		}
+
+		Vector(int size, T value) : _size(size), _vector(new T[_size]) {
+			for (int i = 0; i < _size; i++) {
+				_vector[i] = value;
+			}
+		}
+
+		Vector(int size, T* value) : _size(size), _vector(new T[_size]) {
+			for (int i = 0; i < _size; i++) {
+				_vector[i] = value[i];
+			}
+		}
+
+		Vector(const Vector<T>& vec) : _size(vec.get_size()), _vector(new T[_size]) {
+			for (int i = 0; i < _size; i++) {
+				_vector[i] = vec[i];
+			}
+		}
+
+		Vector(int size, int up, int down) : _size(size), _vector(new T[_size]) {
+			for (int i = 0; i < _size; i++) {
+				_vector[i] = rand() % (up - down) + down + 0.00001 * rand();
+			}
+		}
+
+		Vector<T>& operator+=(const Vector<T>& rhs) {
+			if (_size != rhs.get_size()) {
+				throw exception("Two vectors of different sizes were transmitted");
+			}
+			for (int i = 0; i < _size; i++) {
+				_vector[i] += rhs[i];
+			}
+			return *this;
+		}
+
+		Vector<T> operator+(Vector<T> rhs) const {
+			if (_size != rhs.get_size()) {
+				throw exception("Two vectors of different sizes were transmitted");
+			}
+			return rhs += *this;
+		}
+
+		Vector<T>& operator-=(const Vector<T>& rhs) {
+			if (_size != rhs.get_size()) {
+				throw exception("Two vectors of different sizes were transmitted");
+			}
+			for (int i = 0; i < _size; i++) {
+				_vector[i] -= rhs[i];
+			}
+			return *this;
+		}
+
+		Vector<T> operator-(Vector rhs) const {
+			Vector<T> a(*this);
+			if (_size != rhs.get_size()) {
+				throw exception("Two vectors of different sizes were transmitted");
+			}
+			return a -= rhs;
+		}
+
+		Vector<T>& operator*=(const T a) {
+			for (int i = 0; i < _size; i++) {
+				_vector[i] *= a;
+			}
+			return *this;
+		}
+
+		Vector<T> operator*(T rhs) const {
+			Vector<T> b(*this);
+			return b *= rhs;
+		}
+
+		T operator*(const Vector<T>& rhs) {
+			if (_size != rhs.get_size()) {
+				throw exception("Two vectors of different sizes were transmitted");
+			}
+			T k = 0;
+			for (int i = 0; i < _size; i++) {
+				k += _vector[i] * rhs[i];
+			}
+			return k;
+		}
+
+		Vector<T>& operator/=(const T a) {
+			if (a == 0) {
+				throw exception("Division by zero");
+			}
+			for (int i = 0; i < _size; i++) {
+				_vector[i] /= a;
+			}
+			return *this;
+		}
+
+		Vector<T> operator/(T rhs) const {
+			Vector<T> b(*this);
+			return b /= rhs;
+		}
+
+	};
+
+	template<>
+	Vector<complex<float>>::Vector() : _size(2), _vector(new complex<float>[_size]) {
 		for (int i = 0; i < _size; i++) {
-			_vector[i] = rhs[i];
+			_vector[i] = complex<float>(rand() + 0.00001 * rand(), rand() + 0.00001 * rand());
 		}
 	}
 
-	T& operator[](int i) const {
-		return _vector[i];
-	}
-
-	Vector() : _size(2), _vector(new T[_size]) {
+	template<>
+	Vector<complex<float>>::Vector(int size, int up, int down) : _size(size), _vector(new complex<float>[_size]) {
 		for (int i = 0; i < _size; i++) {
-			_vector[i] = rand() + 0.000001 * rand();
+			_vector[i] = complex<float>(rand() % (up - down) + down + 0.00001 * rand(), rand() % (up - down) + down + 0.00001 * rand());
 		}
 	}
 
-	Vector(int size, T value) : _size(size), _vector(new T[_size]) {
-		for (int i = 0; i < _size; i++) {
-			_vector[i] = value;
+	template<>
+	complex<float> Vector<complex<float>>::operator*(const Vector<complex<float>>& rhs) {
+		if (_size != rhs.get_size()) {
+			throw exception("Two vectors of different sizes were transmitted");
 		}
-	}
-
-	Vector(int size, T* value) : _size(size), _vector(new T[_size]) {
+		complex<float> k = 0;
 		for (int i = 0; i < _size; i++) {
-			_vector[i] = value[i];
-		}
-	}
-
-	Vector(const Vector<T>& vec) : _size(vec.get_size()), _vector(new T[_size]) {
-		for (int i = 0; i < _size; i++) {
-			_vector[i] = vec[i];
-		}
-	}
-
-	Vector(int size, int up, int down) : _size(size), _vector(new T[_size]) {
-		for (int i = 0; i < _size; i++) {
-			_vector[i] = rand() % (up - down) + down + 0.00001 * rand();
-		}
-	}
-
-	Vector<T>& operator+=(const Vector<T>& rhs) {
-		for (int i = 0; i < _size; i++) {
-			_vector[i] += rhs[i];
-		}
-		return *this;
-	}
-
-	Vector<T> operator+(Vector<T> rhs) const {
-		return rhs += *this;
-	}
-
-	Vector<T>& operator-=(const Vector<T>& rhs) {
-		for (int i = 0; i < _size; i++) {
-			_vector[i] -= rhs[i];
-		}
-		return *this;
-	}
-
-	Vector<T> operator-(Vector rhs) const {
-		return rhs -= *this;
-	}
-
-	Vector<T>& operator*=(const T a) {
-		for (int i = 0; i < _size; i++) {
-			_vector[i] *= a;
-		}
-		return *this;
-	}
-
-	Vector<T> operator*(T rhs) const {
-		Vector<T> b(*this);
-		return b *= rhs;
-	}
-
-	T operator*(const Vector<T>& rhs) {
-		T k = 0;
-		for (int i = 0; i < _size; i++) {
-			k += _vector[i] * rhs[i];
+			k += _vector[i] * conj(rhs[i]);
 		}
 		return k;
 	}
 
-	Vector<T>& operator/=(const T a) {
+	template<>
+	Vector<complex<double>>::Vector() : _size(2), _vector(new complex<double>[_size]) {
+		for (int i = 0; i < _size; i++) {
+			_vector[i] = complex<double>(rand() + 0.00001 * rand(), rand() + 0.00001 * rand());
+		}
+	}
+
+	template<>
+	Vector<complex<double>>::Vector(int size, int up, int down) : _size(size), _vector(new complex<double>[_size]) {
+		for (int i = 0; i < _size; i++) {
+			_vector[i] = complex<double>(rand() % (up - down) + down + 0.00001 * rand(), rand() % (up - down) + down + 0.00001 * rand());
+		}
+	}
+
+	template<>
+	complex<double> Vector<complex<double>>::operator*(const Vector<complex<double>>& rhs) {
+		if (_size != rhs.get_size()) {
+			throw exception("Two vectors of different sizes were transmitted");
+		}
+		complex<double> k = 0;
+		for (int i = 0; i < _size; i++) {
+			k += _vector[i] * conj(rhs[i]);
+		}
+		return k;
+	}
+
+	template<>
+	Vector<complex<float>>& Vector<complex<float>>::operator/=(const complex<float> a) {
+		if (a.imag() == 0 && a.real() == 0) {
+			throw exception("Division by zero");
+		}
 		for (int i = 0; i < _size; i++) {
 			_vector[i] /= a;
 		}
 		return *this;
 	}
 
-	Vector<T> operator/(T rhs) const {
-		Vector<T> b(*this);
-		return b /= rhs;
+	template<>
+	Vector<complex<double>>& Vector<complex<double>>::operator/=(const complex<double> a) {
+		if (a.imag() == 0 && a.real() == 0) {
+			throw exception("Division by zero");
+		}
+		for (int i = 0; i < _size; i++) {
+			_vector[i] /= a;
+		}
+		return *this;
 	}
-};
 
-template<>
-Vector<complex<float>>::Vector() : _size(2), _vector(new complex<float>[_size]) {
-	for (int i = 0; i < _size; i++) {
-		_vector[i] = complex<float>(rand() + 0.00001 * rand(), rand() + 0.00001 * rand());
+	template<>
+	bool Vector<complex<float>>::operator==(const Vector<complex<float>> rhs) {
+		if (_size != rhs.get_size()) {
+			throw exception("Two vectors of different sizes were transmitted");
+		}
+		for (int i = 0; i < _size; i++) {
+			if (abs(_vector[i].imag() - rhs[i].imag()) > EPSILION || abs(_vector[i].real() - rhs[i].real()) > EPSILION) {
+				return false;
+			}
+		}
+		return true;
 	}
-}
 
-template<>
-Vector<complex<float>>::Vector(int size, int up, int down) : _size(size), _vector(new complex<float>[_size]) {
-	for (int i = 0; i < _size; i++) {
-		_vector[i] = complex<float>(rand() % (up - down) + down + 0.00001 * rand(), rand() % (up - down) + down + 0.00001 * rand());
+	template<>
+	bool Vector<complex<double>>::operator==(const Vector <complex<double>> rhs) {
+		if (_size != rhs.get_size()) {
+
+			throw exception("Two vectors of different sizes were transmitted");
+		}
+		for (int i = 0; i < _size; i++) {
+			if ((abs(_vector[i].imag() - rhs[i].imag()) > EPSILION) || (abs(_vector[i].real() - rhs[i].real()) > EPSILION)) {
+				return false;
+			}
+		}
+		return true;
 	}
-}
 
-template<>
-complex<float> Vector<complex<float>>::operator*(const Vector<complex<float>>& rhs) {
-	complex<float> k = 0;
-	for (int i = 0; i < _size; i++) {
-		k += _vector[i] * conj(rhs[i]);
+	template <typename T>
+	Vector<T> operator*(T lhs, const Vector<T> rhs) {
+		return rhs * lhs;
 	}
-	return k;
-}
 
-template<>
-Vector<complex<double>>::Vector() : _size(2), _vector(new complex<double>[_size]) {
-	for (int i = 0; i < _size; i++) {
-		_vector[i] = complex<double>(rand() + 0.00001 * rand(), rand() + 0.00001 * rand());
+	template<typename T>
+	std::ostream& operator<<(std::ostream& stream, const Vector<T>& a) {
+		for (int i = 0; i < a.get_size(); i++) {
+			cout << a[i] << " ";
+		}
+		return stream;
 	}
-}
 
-template<>
-Vector<complex<double>>::Vector(int size, int up, int down) : _size(size), _vector(new complex<double>[_size]) {
-	for (int i = 0; i < _size; i++) {
-		_vector[i] = complex<double>(rand() % (up - down) + down + 0.00001 * rand(), rand() % (up - down) + down + 0.00001 * rand());
+	template <typename T>
+	Vector<T> bisector(Vector<T> lhs, Vector<T> rhs) {
+		return ((lhs + rhs) / 2.0);
 	}
-}
 
-template<>
-complex<double> Vector<complex<double>>::operator*(const Vector<complex<double>>& rhs) {
-	complex<double> k = 0;
-	for (int i = 0; i < _size; i++) {
-		k += _vector[i] * conj(rhs[i]);
-	}
-	return k;
 }
-
-template <typename T>
-Vector<T> operator*(T rhs, const Vector<T> lhs) {
-	return lhs * rhs;
-}
-
-template <typename T>
-Vector<T> bes(Vector<T> lhs, Vector<T> rhs) {
-	return ((lhs + rhs) / 2);
-}
-
-template<typename T>
-std::ostream& operator << (std::ostream& stream, const Vector<T>& a) {
-	for (int i = 0; i < _size; i++) {
-		cout << _vector[i] << endl;
-	}
-	return stream;
-}
-
---
+++ test/vector_tests.cc
@@ -0,0 +1,38 @@
+#include <vector/vector.h>
+#include <complex>
+
+using namespace vector;
+
+
+int main() {
+
+	float val_a[2] = { 1.9, 2.8 }, val_b[2] = { 3.4, 5.7 };
+	Vector<float> a(2, val_a), b(2, val_b);
+	cout << "Vector a: {" << a << "}" << "Vector b: {" << b << "}" << endl;
+	cout << "Bisector - " << bisector(a, b) << endl;
+	cout << "Sum " << a + b << endl;
+	cout << "Difference " << a - b << endl;
+	cout << "Multiplication of vectors " << a * b << endl;
+	cout << "Division " << a / 2 << endl;
+	cout << "Multiplication by scalar " << a * 2 << endl;
+	cout << "Multiplication by scalar " << (float)2 * a << endl;
+
+	complex<float> val_c[2] = { complex<float>(1.3, 5.6), complex<float>(4.9, 7.4) }, val_d[2] = { complex<float>(3.2, 2.8), complex<float>(6.7, 2.4) };
+	Vector<complex<float>> c(2, val_c), d(2, val_d);
+	cout << "Vector c: {" << c << "}" << "Vector d: {" << d << "}" << endl;
+	cout << "Bisector - " << bisector(c, d) << endl;
+	cout << "Sum " << c + d << endl;
+	cout << "Difference " << c - d << endl;
+	cout << "Multiplication of vectors " << c * d << endl;
+	cout << "Division " << c / 2 << endl;
+	cout << "Multiplication by scalar " << c * 2 << endl;
+	cout << "Multiplication by scalar " << (complex<float>)(9.4, 4.3) * c << endl;
+
+	cout << (a == a) << endl;
+	cout << (a == b) << endl;
+
+	cout << (c == c) << endl;
+	cout << (c == d) << endl;
+
+	return 0;
+}
